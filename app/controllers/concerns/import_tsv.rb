5d58e2e48507f10cb2c8caaa7fc08ebdb5174897
Jose <pastorpedraza3@gmail.com>
2021-07-03 10:19:33 -0500

    Add test for TSV import

--- app/controllers/concerns/import_tsv.rb
+++ app/controllers/concerns/import_tsv.rb
@@ -6,14 +6,11 @@ class ImportTsv < ApplicationController
 
   def save
 
-
     CSV.foreach(@data) do |row|
 
       row = row[0].split(/(\t)/).select { |w| w != "\t" }
       Row.create(buyer: row[0], item_description: row[1], item_price: row[2], item_total: row[3], seller_address: row[4], seller: row[5], table_id: @table_id)
       
     end
-
-
   end
 end
