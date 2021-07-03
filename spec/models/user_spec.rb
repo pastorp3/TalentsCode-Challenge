7f25eaaf22be00025ec33cd3bb784caa9af2b4b4
Jose <pastorpedraza3@gmail.com>
2021-07-03 09:11:08 -0500

    Add test for table model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,15 +4,20 @@ RSpec.describe User, type: :model do
 
   user = described_class.new(email: 'test@example.com', password: '123456')
 
-  it "It should pass with valid attributes" do
-    expect(user).to be_valid
-  end
-  it "It should fail without a email" do
-    user.email = nil
-    expect(user).to_not be_valid
-  end
-  it "It should fail without password" do
-    user.password = nil
-    expect(user).to_not be_valid
+  describe "User model test" do
+
+    it "It should pass with valid attributes" do
+      expect(user).to be_valid
+    end
+
+    it "It should fail without a email" do
+      user.email = nil
+      expect(user).to_not be_valid
+    end
+    
+    it "It should fail without password" do
+      user.password = nil
+      expect(user).to_not be_valid
+    end
   end
 end
